115929b58313030a0ce2baade3aaa00c5a87a7df
Valentin Vergilov <valyo01996@gmail.com>
2023-05-24 17:14:17 +0300

    Index and register templates with css and some js

---
+++ src/main/java/com/example/EventForgeFrontend/EventForgeFrontendApplication.java
@@ -0,0 +1,14 @@
+package com.example.EventForgeFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventForgeFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventForgeFrontendApplication.class, args);
+
+	}
+
+}
